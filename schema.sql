81f2477bc8ea0f03c4f2fc5266d12b3cc175457c
Tuomas Jani Rudolf Laakso <tuomasl@Tuomass-MacBook-Pro.local>
2024-09-22 17:24:40 +0300

    schema

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE transaction(id SERIAL PRIMARY KEY, amount INT,fund_id INTEGER REFERENCES fund , user_id INTEGER REFERENCES users, sent_date DATE);
+
+
+CREATE TABLE fund(id SERIAL PRIMARY KEY, intrest INT, fund_name TEXT, user_id INTEGER REFERENCES users, create_date DATE) 
+
+
+CREATE TABLE users(id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
+
+
+
